c41313723f1a42c717e4f6aafffc4f6ec4da8454
dfoakley <dfoakley@hotmail.com>
2021-09-19 13:16:58 -0500

    added stylesheet, started make-reservation page

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use(session({
 //	apply body-parser middleware to all incoming requests
 app.use(bodyparser());
 app.use(bodyparser.json());
+app.use('/client', express.static(__dirname + '/client'));
 
 //	read in XML config file
 var text = fs.readFileSync("./dbconfig.xml").toString("utf-8");
@@ -57,11 +58,31 @@ var con = mysql.createPool(cfg);
 //	server listens on port 9001 for incoming connections
 module.exports = app.listen(process.env.PORT || 9001, () => console.log("Listening on port 9001!"));
 
-//	POST method to make reservation
-app.post("/make-reservation", function(req, res)
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/client/landingpage.html");
+});
+
+app.get("/landingpage", function(req, res) {
+	res.sendFile(__dirname + "/client/landingpage.html");
+});
+
+app.get("/make-reservation", function(req, res) {
+	res.sendFile(__dirname + "/client/make-reservation.html");
+});
+
+//	GET method to get availability around requested time
+app.get("/get-availability", function(req, res)
 {
-	var user = req.session.username;	//	TO DO: enable guest session
+	var numGuests = req.body.numGuests;
+	var targetDate = req.body.targetDate;
+	var targetTime = req.body.targetTime;
+});
+
+//	POST method to make reservation
+app.post("/push-reservation", function(req, res)
+{
+	var user = req.session.username;	//	TO DO: enable guest session, maybe something with req.session.id
 	var numGuests = req.body.numGuests;
 	var resvDateTime = req.body.revDateTime;
-	var 
-}
+	var ccNumEncrypt = cryptoJS.sha256.encrypt(req.body.ccNumRaw, cfg.password[0]);		//	TO DO: probably should do some encryption on the frontend before it hits here
+});
